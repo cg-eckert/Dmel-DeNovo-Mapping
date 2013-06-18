f0872bce50c87b8a79951ff77a82f7ddccb6a408
Chris Eckert <chris.eckert@queensu.ca>
2013-06-18 09:42:25 -0300

    Update DmelDeNovoMappingScript.sh

--- DmelDeNovoMappingScript.sh
+++ DmelDeNovoMappingScript.sh
@@ -62,7 +62,8 @@ bowtie denovo_bowtie w.trimmed.fq.2 w.2.sam
 
 # Convert SAM files to indexed BAM files
 echo "******converting SAM to BAM********"
-samtools faidx /data/dmel_trinity/Trinity.fasta
+cp /data/dmel_trinity/Trinity.fasta /mnt/map/Trinity.fasta
+samtools faidx /mnt/map/Trinity.fasta
 samtools view -bt /data/dmel_trinity/Trinity.fasta.fai vg.1.sam > vg.1.temp.bam
 samtools view -bt /data/dmel_trinity/Trinity.fasta.fai vg.2.sam > vg.2.temp.bam
 samtools view -bt /data/dmel_trinity/Trinity.fasta.fai w.1.sam > w.1.temp.bam
