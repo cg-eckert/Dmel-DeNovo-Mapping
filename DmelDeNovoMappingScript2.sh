54948f574d54ecfbffc3f85ae9568586177a2f4a
Chris Eckert <chris.eckert@queensu.ca>
2013-06-18 17:03:58 -0300

    Update DmelDeNovoMappingScript2.sh

--- DmelDeNovoMappingScript2.sh
+++ DmelDeNovoMappingScript2.sh
@@ -3,6 +3,8 @@ cd /
 #mkdir /data
 #mount -o ro /dev/xvdf /data
 
+mkdir /mnt/map
+
 #The de novo assembly is in the file /data/dmel_trinity/Trinity.fasta
 
 #The read fastq files are in /data
@@ -11,20 +13,30 @@ cd /
 #DATA3=/data/OREf_SAMm_w_GTCCGC_L006_R1_001.fastq
 #DATA4=/data/OREf_SAMm_w_GTCCGC_L006_R2_001.fastq
 
+cd /data
+seqtk sample -s 11 OREf_SAMm_vg1_CTTGTA_L005_R1_001.fastq 10000 > /mnt/map/vg1.fastq
+seqtk sample -s 11 OREf_SAMm_vg1_CTTGTA_L005_R2_001.fastq 10000 > /mnt/map/vg2.fastq
+seqtk sample -s 11 OREf_SAMm_w_GTCCGC_L006_R1_001.fastq 10000 > /mnt/map/w1.fastq
+seqtk sample -s 11 OREf_SAMm_w_GTCCGC_L006_R2_001.fastq 10000 > /mnt/map/w2.fastq
+
+
 DATA1=/mnt/map/vg1.fastq
 DATA2=/mnt/map/vg2.fastq
 DATA3=/mnt/map/w1.fastq
 DATA4=/mnt/map/w2.fastq
 
 #Start with some read QC
-#mkdir /mnt/QC
-#/usr/local/share/FastQC/fastqc /data/*.fastq --outdir=/mnt/QC
+cd /mnt
+rm -r QC
+mkdir /mnt/QC
+/usr/local/share/FastQC/fastqc /data/*.fastq --outdir=/mnt/QC
 
 #All other files will be written to /mnt/map
+
 mkdir /mnt/map
 
 #Interleave the paired end reads in preparation for fastx trimming
-cd /data
+
 python /usr/local/share/khmer/sandbox/interleave.py $DATA1 $DATA2 > /mnt/map/vg.combined.fastq
 python /usr/local/share/khmer/sandbox/interleave.py $DATA3 $DATA4 > /mnt/map/w.combined.fastq
 
@@ -64,15 +76,14 @@ echo "********* SAMs have been BAMed *************"
 # Using bedtools to calculate read counts
 echo "********bedtools analysis starting*********"
 cd /mnt/map
-cp /data/dmel_trinity/final_dmel.bed final_dmel.bed
-sed '$d' final_dmel.bed
-for i in vg w ; do
-  bedtools coverage -s -abam $i.bam -b /data/dmel_trinity/final_dmel.bed > $i.counts.txt
-done
+cp /data/dmel_trinity/final_dmel.bed temp_dmel.bed
+sed '$d' temp_dmel.bed > final_dmel.bed
+bedtools multicov -q 30 -p -bams vg.bam w.bam -bed final_dmel.bed > transcriptome_counts.txt
 
 echo "********bedtools analysis FINISHED********"
 
-R
+R --no-save < Rscript.R
+
 
 
 
